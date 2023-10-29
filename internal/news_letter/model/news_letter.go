acec7f8daa24f1875938255a8fcb8ec3d9572e1a
suyogkokaje <suyogkokaje6@gmail.com>
2023-10-29 14:43:20 +0530

    fix:cascade delete

--- internal/news_letter/model/news_letter.go
+++ internal/news_letter/model/news_letter.go
@@ -2,13 +2,16 @@ package model
 
 import (
 	edition_model "go_newsletter_api/internal/edition/model"
+	user_model "go_newsletter_api/internal/user/model"
 )
 
 type Newsletter struct {
-	ID       uint                    `json:"id"`
-	Name     string                  `json:"name"`
-	AdminID  uint                    `json:"admin_id"`
-	Editions []edition_model.Edition `gorm:"foreignkey:NewsletterID;constraint:OnDelete:CASCADE"`
+	ID          uint                    `json:"id"`
+	Name        string                  `json:"name"`
+	AdminID     uint                    `json:"admin_id"`
+	Editions    []edition_model.Edition `gorm:"foreignkey:NewsletterID;constraint:OnDelete:CASCADE"`
+	Subscribers []NewsletterSubscriber  `gorm:"foreignKey:NewsletterID;references:ID;constraint:OnDelete:CASCADE"`
+	User        user_model.User         `gorm:"foreignKey:AdminID;references:ID;constraint:OnDelete:CASCADE"`
 }
 
 type NewsletterSubscriber struct {
